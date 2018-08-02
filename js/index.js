95baa95e0ef0bfb71db18dae96c3e74d3f74c87f
Wang-Vei <wangweilove@qq.com>
2018-08-02 09:48:49 +0800

    sd

---
+++ js/index.js
@@ -0,0 +1,23 @@
+/*
+* @Author: Sing-V
+* @Date:   2018-07-30 22:16:58
+* @Last Modified by:   Sing-V
+* @Last Modified time: 2018-07-30 23:10:46
+*/
+window.onload=function () {
+	let pull1=document.querySelector(".pull1");
+	let zhi=document.querySelector(".zhi")
+	console.log(pull1,zhi);	
+
+	zhi.onmouseenter=function(){
+		pull1.style.height="400px";
+		pull1.style.border="1px solid #ccc"
+	}
+	zhi.onmouseleave=function(){
+		pull1.style.height="0";
+		pull1.style.border="";
+	}
+
+
+
+}
